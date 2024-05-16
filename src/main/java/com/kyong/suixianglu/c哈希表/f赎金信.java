bf4d0523f06ce4eb2c1443fbf4cde812c6d47533
kyong <kyong@fiberhome.com>
2024-05-16 20:31:31 +0800

    5.16

---
+++ src/main/java/com/kyong/suixianglu/c哈希表/f赎金信.java
@@ -0,0 +1,14 @@
+package com.kyong.suixianglu.c哈希表;
+
+/**
+ * Description
+ *
+ * @author kyong
+ * @version 1.0
+ * @date 2024/5/16 20:26
+ */
+public class f赎金信 {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        return false;
+    }
+}
