17dc044652fcabef42ced84334594552bae78ffc
kyong <291031979@qq.com>
2024-06-03 17:32:29 +0800

    6.3

--- src/main/java/com/kyong/suixianglu/e栈与队列/c有效的括号.java
+++ src/main/java/com/kyong/suixianglu/e栈与队列/c有效的括号.java
@@ -1,5 +1,8 @@
 package com.kyong.suixianglu.e栈与队列;
 
+import java.util.LinkedList;
+import java.util.Stack;
+
 /**
  * Description
  *
@@ -9,4 +12,51 @@ package com.kyong.suixianglu.e栈与队列;
  */
 //20.有效的括号
 public class c有效的括号 {
+//    public boolean isValid(String s) {
+//        if(s.length()%2 != 0) return false;
+//        int anum = 0;
+//        int bnum = 0;
+//        int cnum = 0;
+//        for (int i = 0; i < s.length(); i++) {
+//            if (s.charAt(i) == '('){
+//                anum++;
+//            }else if(s.charAt(i) == '['){
+//                bnum++;
+//            }else if(s.charAt(i) == '{'){
+//                cnum++;
+//            }else if(s.charAt(i) == ')'){
+//                anum--;
+//                if(anum<0){
+//                    return false;
+//                }
+//            }else if(s.charAt(i) == ']'){
+//                bnum--;
+//                if(bnum<0){
+//                    return false;
+//                }
+//            }else if(s.charAt(i) == '}'){
+//                cnum--;
+//                if(cnum<0){
+//                    return false;
+//                }
+//            }
+//        }
+//        return true;
+//    }
+    public boolean isValid(String s) {
+        LinkedList<Character> characters1 = new LinkedList<>();
+        Stack<Character> characters = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i)  == '(' || s.charAt(i)=='{' || s.charAt(i) == '['){
+                characters.add(s.charAt(i));
+            }else if (s.charAt(i)==')' && (characters.empty() || characters.pop()!= '(')){
+                return false;
+            }else if(s.charAt(i)==']'&& (characters.empty() ||characters.pop()!= '[')){
+                return false;
+            }else if(s.charAt(i)=='}'&& (characters.empty() ||characters.pop()!= '{')){
+                return false;
+            }
+        }
+        return characters.empty();
+    }
 }
