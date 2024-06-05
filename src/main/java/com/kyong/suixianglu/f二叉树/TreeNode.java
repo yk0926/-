09a95d2dc6818273e3322012e2fbcef6cb4cacc2
kyong <291031979@qq.com>
2024-06-05 17:24:18 +0800

    6.5

---
+++ src/main/java/com/kyong/suixianglu/f二叉树/TreeNode.java
@@ -0,0 +1,24 @@
+package com.kyong.suixianglu.f二叉树;
+
+/**
+ * Description
+ *
+ * @author kyong
+ * @version 1.0
+ * @date 2024/6/5 17:21
+ */
+public class TreeNode {
+    TreeNode() {};
+    TreeNode(int val){
+        this.val = val;
+    }
+    TreeNode(int val, TreeNode left, TreeNode right){
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+}
